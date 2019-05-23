19f6b091bbe869ab24be69c5cd2b128e136460c7
Servio Tulio Reyes Castillo <servio_trc@yahoo.com.mx>
2019-05-23 15:19:39 -0500

    Styling components

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import StyledPlaylisttItem from './styles/StyledPlaylistItem';
 
-const PlaylistItem = props => (
-        <div>
-            PlaylistItem!!!
-        </ div>
+const PlaylistItem = ({video, active, played}) => (
+        <StyledPlaylisttItem active={active} played={played}>
+            <div className="wbn-player__video-nr">{video.num}</div>
+            <div className="wbn-player__video-name">{video.title}</div>
+            <div className="wbn-player__video-time ">{video.duration }</div>
+        </ StyledPlaylisttItem>
     )
 
 export default PlaylistItem;
--- src/components/containers/BogoPlayer.js
+++ src/components/containers/BogoPlayer.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import {ThemeProvider} from 'styled-components';
 import Video from '../Video';
 import Playlist from '../containers/Playlist';
+import StyledBogoPlayer from '../styled/StyledBogoPlayer';
+
+const theme = {
+    bgcolor: "#353535",
+    bgcolorItem: "#414141",
+    bgcolorItemActive: "#405c63",
+    bgcolorPlay: "#526d4e",
+    border: "none",
+    borderPlayed: "none",
+    color: "#fff"
+};
+
+const themeLight = {
+    bgcolor: "#fff",
+    bgcolorItem: "#fff",
+    bgcolorItemActive: "#80a7b1",
+    bgcolorPlay: "#526d4e",
+    border: "1px solid #353535",
+    borderPlayed: "none",
+    color: "#353535"
+};
 
 const BogoPlayer = props => {
     return (
-        <>
-        <Video />
-        <Playlist />
-        </>
+        <ThemeProvider theme={state.nightMode ? theme : themeLight}>
+            <StyledBogoPlayer>
+                <Video />
+                <Playlist />
+            </StyledBogoPlayer>
+        </ThemeProvider>
     )
 }
 

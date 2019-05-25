14101b828415642029a849eb1734422bb227e15f
Servio Tulio Reyes Castillo <servio_trc@yahoo.com.mx>
2019-05-25 14:49:21 -0500

    Hooks usage

--- src/components/containers/BogoPlayer.js
+++ src/components/containers/BogoPlayer.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {ThemeProvider} from 'styled-components';
 import Video from '../Video';
 import Playlist from '../containers/Playlist';
-import StyledBogoPlayer from '../styled/StyledBogoPlayer';
+import StyledBogoPlayer from '../styles/StyledBogoPlayer';
 
 const theme = {
     bgcolor: "#353535",
@@ -25,12 +25,40 @@ const themeLight = {
 };
 
 const BogoPlayer = props => {
+
+    const videos = JSON.parse(document.querySelector('[name="videos"]').value);
+
+    const [state, setState] = useState({
+        videos: videos.playlist,
+        activeVideo: videos.playlist[0],
+        nightMode: true,
+        playlistId: videos.playlistId,
+        autoplay: false,
+    });
+
+    const nightModeCallback = () => {}
+    const endCallback = () => {}
+    const progressCallback = () => {}
+
+
     return (
         <ThemeProvider theme={state.nightMode ? theme : themeLight}>
-            <StyledBogoPlayer>
-                <Video />
-                <Playlist />
-            </StyledBogoPlayer>
+            {state.videos !== null ? (
+                <StyledBogoPlayer>
+                    <Video 
+                        active={state.activeVideo}
+                        autoplay={state.autoplay}
+                        endCallback={endCallback}
+                        progressCallback={progressCallback}
+                    />
+                    <Playlist
+                        videos={state.videos}
+                        active={state.activeVideo}
+                        nightModeCallback={nightModeCallback}
+                        nightMode={state.nightMode}
+                    />
+                </StyledBogoPlayer>
+            ) : null}
         </ThemeProvider>
     )
 }

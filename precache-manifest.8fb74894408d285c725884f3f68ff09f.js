d1cb177853104c2a3cfb1e0756f97994fe5bb4ee
Servio Tulio Reyes Castillo <servio_trc@yahoo.com.mx>
2019-05-29 14:47:43 -0500

    Updates

---
+++ precache-manifest.8fb74894408d285c725884f3f68ff09f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c4371d5fcfb71c6dbd2",
+    "url": "/React-Web-VideoPlayer/static/js/main.3c4371d5.chunk.js"
+  },
+  {
+    "revision": "612383bb75997fb02a69",
+    "url": "/React-Web-VideoPlayer/static/js/runtime~main.612383bb.js"
+  },
+  {
+    "revision": "e13cc2ed727ad4cb4b4e",
+    "url": "/React-Web-VideoPlayer/static/js/2.e13cc2ed.chunk.js"
+  },
+  {
+    "revision": "4948422b9e6eec0af45492e0035cb570",
+    "url": "/React-Web-VideoPlayer/index.html"
+  }
+];
